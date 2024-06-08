b4e34e1b41c2dc6a62f3a4b685cf2008d1b30db3
JerryHVK <khanghoangvan29042003@gmail.com>
2024-06-09 01:13:42 +0700

    db_connect

--- System/db/db_connection.py
+++ System/db/db_connection.py
@@ -2,14 +2,14 @@ import pyodbc as pdb
 
 ### Connect to SQL Server database ###
 # Connect to SQL Server database info
-SERVER = 'DUYNGUYEN\SQLEXPRESS'
+SERVER = 'DESKTOP-NESGTGV\SQLEXPRESS'
 DATABASE = 'Project'
 USERNAME = 'sa'
 PASSWORD = '123456'
 
 # Connection string
 connectionString = f'''
-DRIVER={{ODBC Driver 18 for SQL Server}};
+DRIVER={{ODBC Driver 17 for SQL Server}};
 SERVER={SERVER};DATABASE={DATABASE};
 UID={USERNAME};
 PWD={PASSWORD};
